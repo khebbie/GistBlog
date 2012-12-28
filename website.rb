97bf3455ec55c9d293adac0c8b952f07de392de4
Klaus Hebsgaard <klaus@hebsgaard.dk>
2012-12-28 15:58:52 +0100

    extracted classes

--- website.rb
+++ website.rb
@@ -4,20 +4,48 @@ require 'haml'
 require 'open-uri'
 require 'json'
 
+class GistsAPI
+  def gists_for_user()
+     uri = "https://api.github.com/users/khebbie/gists"
+     content = open(uri).read
+     parsed = JSON.parse(content)
+     
+     filename = ""
+     parsed["files"].each do |gistfile|
+       content = gistfile[1]["content"].to_s
+       filename =  gistfile[1]["filename"].to_s
+       filename = File.basename( filename, ".*" )
+     end
+  end
+
+  def gist(id)
+    uri = "https://api.github.com/gists/" + id
+    content = open(uri).read
+    parsed = JSON.parse(content)
+    filename = ""
+  
+    parsed["files"].each do |gistfile|
+      content = gistfile[1]["content"].to_s
+      filename =  gistfile[1]["filename"].to_s
+      filename = File.basename( filename, ".*" )
+    end
+    gist = Gist.new
+    gist.filename = filename
+    gist.content = content
+    gist
+  end
+end
+
+class Gist
+  attr_accessor :content
+  attr_accessor :filename
+end
+
 get '/gist/:id' do  
   id = params[:id]
-  uri = "https://api.github.com/gists/" + id
-  content = open(uri).read
-  parsed = JSON.parse(content)
-  filename = ""
-  
-  parsed["files"].each do |gistfile|
-    content = gistfile[1]["content"].to_s
-    filename =  gistfile[1]["filename"].to_s
-    filename = File.basename( filename, ".*" )
-  end
-  
-  haml :index  , :locals => {:content => content, :filename => filename}
+  gistsAPI = GistsAPI.new()
+  gist = gistsAPI.gist(id)
+  haml :index  , :locals => {:content => gist.content, :filename => gist.filename}
 end
 
 get '/' do
