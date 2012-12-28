5f0c78eb99bc416f0c1dbdec89af94816e7b4559
Klaus Hebsgaard <klaus@hebsgaard.dk>
2012-12-28 15:34:27 +0100

    added title to blogpost

--- website.rb
+++ website.rb
@@ -9,12 +9,15 @@ get '/gist/:id' do
   uri = "https://api.github.com/gists/" + id
   content = open(uri).read
   parsed = JSON.parse(content)
-
+  filename = ""
+  
   parsed["files"].each do |gistfile|
     content = gistfile[1]["content"].to_s
+    filename =  gistfile[1]["filename"].to_s
+    filename = File.basename( filename, ".*" )
   end
   
-  haml :index  , :locals => {:content => content}
+  haml :index  , :locals => {:content => content, :filename => filename}
 end
 
 get '/' do
