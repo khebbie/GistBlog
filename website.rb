58f5089bffb7727a7bd9a872fc851f3ed37085db
Klaus Hebsgaard <klaus@hebsgaard.dk>
2012-12-30 23:36:38 +0100

    finally working again except titles has file endings

--- website.rb
+++ website.rb
@@ -4,6 +4,12 @@ require 'haml'
 require 'open-uri'
 require 'json'
 
+Gist = Struct.new(:content, :filename, :title, :id) do
+  def empty?()
+    false
+  end
+end
+
 class GistsAPI
   def gists_for_user(username)
     uri = "https://api.github.com/users/" + username +"/gists"
@@ -12,55 +18,48 @@ class GistsAPI
 
     gists = Array.new
 
-    parsed.each do |gistJson|
+   parsed.each do |gistJson|
       id = gistJson["id"]
-      gist = parse_gist(gistJson)
-        gist.id = id
-        next if File.extname(gist.filename) != ".md"
-        gists << gist
-      end
+      gist = parse_gist(gistJson, id)
+      p gist
+      next if gist.empty?
+      filename = gist.filename
+      next if File.extname(filename) != ".md"
+
+      gists << gist
+    end
     gists
   end
 
-  def gist(id)
+  def gist_by(id)
     uri = "https://api.github.com/gists/" + id
     content = open(uri).read
     parsed = JSON.parse(content)
     
     parse_gist(parsed)
   end
-  def parse_gist(json)
-     filename = ""
-     content = ""
-     title = ""
+  def parse_gist(json, id = "0")
     json["files"].each do |gistfile|
+
       content = gistfile[1]["content"].to_s
       filename =  gistfile[1]["filename"].to_s
       title = File.basename( filename, ".*" )
+      
+      return Gist.new(content, filename, title, id)
     end
-    gist = Gist.new
-    gist.filename = filename
-    gist.content = content
-    gist.title = title
-    gist
-
   end
 end
-
-Gist = Struct.new(:content, :filename, :id, :title)
 
 get '/gist/:id' do  
   id = params[:id]
   gistsAPI = GistsAPI.new()
-  gist = gistsAPI.gist(id)
+  gist = gistsAPI.gist_by(id)
   haml :index  , :locals => {:content => gist.content, :filename => gist.filename}
 end
 
 get '/' do
   gistsAPI = GistsAPI.new()
   @gists = gistsAPI.gists_for_user("khebbie")
-
-
   haml :home
 end
 
