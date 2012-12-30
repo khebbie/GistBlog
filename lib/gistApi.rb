373244e5951e113a8cc00a5f204633d5ebccb553
Klaus Hebsgaard <klaus@hebsgaard.dk>
2012-12-31 00:07:41 +0100

    refactored class into own file

---
+++ lib/gistApi.rb
@@ -0,0 +1,47 @@
+require 'rubygems'  
+require 'open-uri'
+
+Gist = Struct.new(:content, :filename, :title, :id) do
+  def empty?()
+    false
+  end
+end
+
+class GistsAPI
+  def gists_for_user(username)
+    uri = "https://api.github.com/users/" + username +"/gists"
+    content = open(uri).read
+    parsed = JSON.parse(content)
+
+    gists = Array.new
+
+   parsed.each do |gistJson|
+      id = gistJson["id"]
+      gist = parse_gist(gistJson, id)
+      next if gist.empty?
+      filename = gist.filename
+      next if File.extname(filename) != ".md"
+
+      gists << gist
+    end
+    gists
+  end
+
+  def gist_by(id)
+    uri = "https://api.github.com/gists/" + id
+    content = open(uri).read
+    json = JSON.parse(content)
+    
+    parse_gist(json)
+  end
+  def parse_gist(json, id = "0")
+    json["files"].each do |gistfile|
+
+      content = gistfile[1]["content"].to_s
+      filename =  gistfile[1]["filename"].to_s
+      title = File.basename( filename, ".*" )
+      
+      return Gist.new(content, filename, title, id)
+    end
+  end
+end
--- website.rb
+++ website.rb
@@ -1,54 +1,8 @@
 require 'rubygems'  
 require 'sinatra'  
 require 'haml'
-require 'open-uri'
 require 'json'
-
-Gist = Struct.new(:content, :filename, :title, :id) do
-  def empty?()
-    false
-  end
-end
-
-class GistsAPI
-  def gists_for_user(username)
-    uri = "https://api.github.com/users/" + username +"/gists"
-    content = open(uri).read
-    parsed = JSON.parse(content)
-
-    gists = Array.new
-
-   parsed.each do |gistJson|
-      id = gistJson["id"]
-      gist = parse_gist(gistJson, id)
-      p gist
-      next if gist.empty?
-      filename = gist.filename
-      next if File.extname(filename) != ".md"
-
-      gists << gist
-    end
-    gists
-  end
-
-  def gist_by(id)
-    uri = "https://api.github.com/gists/" + id
-    content = open(uri).read
-    json = JSON.parse(content)
-    
-    parse_gist(json)
-  end
-  def parse_gist(json, id = "0")
-    json["files"].each do |gistfile|
-
-      content = gistfile[1]["content"].to_s
-      filename =  gistfile[1]["filename"].to_s
-      title = File.basename( filename, ".*" )
-      
-      return Gist.new(content, filename, title, id)
-    end
-  end
-end
+require './lib/gistApi.rb'
 
 get '/gist/:id' do  
   id = params[:id]
