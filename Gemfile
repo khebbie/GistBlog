82d07ec4ac6f494173febe5077253d01c87f34f5
Klaus Hebsgaard <klaus@hebsgaard.dk>
2013-01-05 09:10:57 +0100

    moved configuration to yml

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', '1.3.3'
+gem 'sinatra-contrib'
 gem "thin", "~> 1.5.0"
 gem "bluecloth", "~> 2.2.0"
 gem "redcarpet", "~> 1.17.2"
--- website.rb
+++ website.rb
@@ -1,9 +1,12 @@
 require 'rubygems'  
 require 'sinatra'  
+require "sinatra/config_file"
 require 'haml'
 require 'json'
 require './lib/gistApi.rb'
 require './lib/gravatar.rb'
+
+config_file 'config.yml'
 
 get '/gist/:id' do  
   id = params[:id]
@@ -14,11 +17,11 @@ end
 
 get '/' do
   gistsAPI = GistsAPI.new()
-  @gists = gistsAPI.gists_for_user("khebbie")
+  @gists = gistsAPI.gists_for_user(settings.githubUsername)
   haml :home
 end
 
 get '/about' do
-  @image_src = Gravatar.new().get_image_url("klaus@hebsgaard.dk")
+  @image_src = Gravatar.new().get_image_url(settings.gravatarEmail)
   haml :about
 end
